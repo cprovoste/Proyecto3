7b0fa9811021f139fc984c8227f99aa4a3602db3
Claudia Provoste Pérez <cprovoste16@alumnos.utalca.cl>
2016-12-11 19:46:40 -0300

    Re- organización de UI

--- Proyecto3/src/proyecto3/UIJuego.java
+++ Proyecto3/src/proyecto3/UIJuego.java
@@ -1,23 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package proyecto3;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
@@ -27,64 +17,69 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import proyecto3.model.CircusCrushCanvas;
+import proyecto3.painter.Cargador;
 /**
  *
  * @author Claudia
  */
-public class UIJuego  extends Stage implements EventHandler, ChangeListener {
+public class UIJuego extends Stage implements EventHandler, ChangeListener {
 
     private Button about;
-    private int score;
-    
-
-    public UIJuego(Button about, int score) {
-        this.about = about;
-        this.score = score;
-    }
+    private Button registro;
+    private Button puntaje;
     
     public UIJuego(){
         
         
-    super.setTitle("Circus Crush");
-    StackPane root = new StackPane();
-        
-    BorderPane panelPrincipal = new BorderPane();
-    root.getChildren().add(panelPrincipal);
-    
-     HBox topPane = new HBox();
-    topPane.setBackground( new Background(new BackgroundFill(new Color(190/255.0, 0, 102/255.0, 1), CornerRadii.EMPTY, Insets.EMPTY)));
-    Image image = new Image(getClass().getResourceAsStream("Sin título1.png"));
-    Label logo = new Label("", new ImageView(image));
-    topPane.getChildren().add(logo);
-    panelPrincipal.setTop(topPane);
+        super.setTitle("Circus Crush");
+        StackPane root = new StackPane();
 
-    BorderPane panelCentral = new BorderPane();
-    
-    
-    
-      BorderPane panelTabla = new BorderPane();
-    // panelTabla.setCenter(table);
-     panelTabla.setPadding(new Insets(0, 0, 10, 0));
-     panelCentral.setCenter(panelTabla);
+        BorderPane panelPrincipal = new BorderPane();
+        root.getChildren().add(panelPrincipal);
 
-     panelPrincipal.setCenter(panelCentral);
+        HBox topPane = new HBox();
+        topPane.setBackground( new Background(new BackgroundFill(new Color(190/255.0, 71/255.0, 71/255.0, 1), CornerRadii.EMPTY, Insets.EMPTY)));
+        Image image = Cargador.getImage("banner.png");
+        Label logo = new Label("", new ImageView(image));
+        topPane.getChildren().addAll(logo);
+        panelPrincipal.setTop(topPane);
 
-     Scene scene = new Scene(root, 800, 600);
-     super.setScene(scene);
+        HBox panelBotones = new HBox(10);
+        this.registro = new Button("registro");
+        this.registro.setPrefWidth(100);
+        this.puntaje = new Button("puntaje");
+        this.puntaje.setPrefWidth(100);
+        panelBotones.getChildren().addAll( this.registro, this.puntaje);
+        panelPrincipal.setBottom(panelBotones);
+
+        CircusCrushCanvas paint = new CircusCrushCanvas();
+        panelPrincipal.setCenter(paint);
+
+        paint.widthProperty().bind(panelPrincipal.widthProperty());
+        paint.heightProperty().bind(panelPrincipal.heightProperty());
+
+        Scene scene = new Scene(root, 800, 600);
+        super.setScene(scene);
+
+        this.registro.setOnAction(this);
+        this.puntaje.setOnAction(this);
     }
     
-    void agregarJugador(Jugador jugador) {
+    public void agregarJugador(Jugador jugador) {
        //
     }
-    
-    
 
-    
     @Override
     public void handle(Event event) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        if( event.getSource() == this.registro )
+        {
+            UIRegistro stage = new UIRegistro(this);
+            stage.show();
+        }
+
     }
 
     @Override
