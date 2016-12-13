6ce568f74d52e7a001c87b226ff3a801dbd4f564
Claudia Provoste Pérez <cprovoste16@alumnos.utalca.cl>
2016-12-12 23:59:06 -0300

    quick change

--- Proyecto3/src/proyecto3/UI/UIJuego.java
+++ Proyecto3/src/proyecto3/UI/UIJuego.java
@@ -3,7 +3,7 @@
  * Esta es la ventana principal donde ocurre el gameplay.
  */
 
-package proyecto3;
+package proyecto3.UI;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.Event;
@@ -11,6 +11,9 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
@@ -18,8 +21,11 @@ import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import proyecto3.Jugador;
 import proyecto3.model.CircusCrushCanvas;
+import proyecto3.painter.Cargador;
 /**
  *
  * @author Claudia Provoste y Samuel Paicil
@@ -39,11 +45,32 @@ public class UIJuego extends Stage implements EventHandler, ChangeListener {
         BorderPane panelPrincipal = new BorderPane();
         root.getChildren().add(panelPrincipal);
 
-        HBox topPane = new HBox();
+        BorderPane topPane = new BorderPane();
         topPane.setBackground( new Background(new BackgroundFill(new Color(190/255.0, 71/255.0, 71/255.0, 1), CornerRadii.EMPTY, Insets.EMPTY)));
-       // Image image = Cargador.getImage("banner.png");
-       // Label logo = new Label("", new ImageView(image));
-       // topPane.getChildren().addAll(logo);
+        topPane.setPadding( new Insets(15) );
+        
+        
+        Label vidas = new Label("Vidas: ");
+        vidas.setTextFill(Color.web("#fff8d6"));
+        vidas.setFont(new Font("Helvetica", 15));
+        //vidas.setFont(Font.loadFont("file:proyecto3/fonts/Lato-Bold.ttf", 120));
+        vidas.setPadding(new Insets(60,0,0,0));
+        
+        
+        Label puntaje = new Label("Puntaje: ");
+        puntaje.setTextFill(Color.web("#fff8d6"));
+        puntaje.setPadding(new Insets(50,0,0,70));
+        
+        Image image = Cargador.getImage("about.png");
+        //Label about = new Label("", new ImageView(image));
+        this.about = new Button();
+        this.about.setGraphic(new ImageView(image));
+        this.about.setPadding(new Insets(50,0,0,0));
+        
+       
+        topPane.setLeft(vidas);
+        topPane.setCenter(puntaje);
+        topPane.setRight(this.about);
         panelPrincipal.setTop(topPane);
 
         HBox panelBotones = new HBox(10);
@@ -51,6 +78,7 @@ public class UIJuego extends Stage implements EventHandler, ChangeListener {
         this.registro.setPrefWidth(100);
         this.puntaje = new Button("puntaje");
         this.puntaje.setPrefWidth(100);
+        panelPrincipal.setBottom(panelBotones);
 
         CircusCrushCanvas paint = new CircusCrushCanvas();
         panelPrincipal.setCenter(paint);
@@ -60,7 +88,8 @@ public class UIJuego extends Stage implements EventHandler, ChangeListener {
 
         Scene scene = new Scene(root, 400, 400);
         super.setScene(scene);
-
+        
+        this.about.setOnAction(this);
         this.registro.setOnAction(this);
         this.puntaje.setOnAction(this);
     }
@@ -75,6 +104,12 @@ public class UIJuego extends Stage implements EventHandler, ChangeListener {
         if( event.getSource() == this.registro )
         {
             UIRegistro stage = new UIRegistro(this);
+            stage.show();
+        }
+        
+        if (event.getSource() == this.about )
+        {
+            UIAbout stage = new UIAbout(this);
             stage.show();
         }
 
